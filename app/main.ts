81d58b476d56e6ab4e9935c486975d1aed1f01e7
monajalal <jalal@wisc.edu>
2016-06-29 18:40:33 -0500

    error

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,10 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
+import {AppComponent} from '@angular/core'
 
+@Component({
+    selector: 'my-app',
+    template: '<h1>Ultra Racing</h1>'
+
+})
+class AppComponent{}
 bootstrap(AppComponent);
